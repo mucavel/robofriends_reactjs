a75a77244685e7295ca92991365cc50d08b073e2
salvadormucavel <salvadormuca7@gmail.com>
2022-05-19 16:43:48 +0200

    reestruturacao de pastas

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 
 
 
