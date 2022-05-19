3da6033397d19a0c714978c3ee31ba9f83818167
salvadormucavel <salvadormuca7@gmail.com>
2022-05-19 14:37:55 +0200

    usuarios remotos

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, {Component} from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from './robots';
+import './App.css'
 
 class App extends Component{
     constructor(){
         super()
         this.state = {
-            robots : robots,
+            robots : [],
             searchfield : ''
         }
     }
-
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots : users}))
+    }
     onSearchChange = (event) =>{
         this.setState({searchfield: event.target.value})
     }
@@ -22,7 +26,7 @@ class App extends Component{
         })
         return(
             <div className="tc">
-                <h1>RoboFriends</h1>
+                <h1 className="robo-h1">RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <CardList robots = {filteredRobots}/>
             </div>
